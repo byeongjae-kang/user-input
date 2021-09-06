2ae912faf23ede4c812dd28c1c74ad7f64d00df1
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-05 23:11:32 -0700

    add basic input and submit functions to change state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [username, setUsername] = useState("");
+
+  const userInputHandler = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUsername('')
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          value={username}
+          onChange={userInputHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
