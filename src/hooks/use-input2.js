b94cf00bb9e5f67c2586b0fa35bf2e99e73a6dc7
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-09-06 19:40:43 -0700

    implement useReducer

--- src/hooks/use-input2.js
+++ src/hooks/use-input2.js
@@ -1,25 +1,53 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const INITIAL_INPUT = {
+  value: "",
+  isEntered: false
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isEntered: state.isEntered };
+  }
+  if (action.type === "BLUR") {
+    return { value: state.value, isEntered: true };
+  }
+  if (action.type === "RESET") {
+    return { value: "", isEntered: false };
+  }
+};
 
 const useInput2 = (validateValue) => {
-  const [value, setValue] = useState("");
-  const [isEntered, setIsEntered] = useState(false);
+  const [inputState, dispatchInput] = useReducer(
+    inputStateReducer,
+    INITIAL_INPUT
+  );
 
-  const IsValid = validateValue(value);
-  const hasError = !IsValid && isEntered;
+  // const [value, setValue] = useState("");
+  // const [isEntered, setIsEntered] = useState(false);
+
+  const IsValid = validateValue(inputState.value);
+  const hasError = !IsValid && inputState.isEntered;
 
   const changeHandler = (e) => {
-    setValue(e.target.value);
+    dispatchInput({ type: "INPUT", value: e.target.value });
   };
   const blurHandler = (e) => {
-    setIsEntered(true);
+    dispatchInput({ type: "BLUR" });
   };
 
   const reset = () => {
-    setValue("");
-    setIsEntered(false);
+    dispatchInput({ type: "RESET" });
   };
 
-  return { value, IsValid, hasError, reset, changeHandler, blurHandler };
+  return {
+    value: inputState.value,
+    IsValid,
+    hasError,
+    reset,
+    changeHandler,
+    blurHandler
+  };
 };
 
 export default useInput2;
